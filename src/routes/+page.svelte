82b56903417294bfb38a34fca1c83c0e126d4095
ikeology <ikeology01123@gmail.com>
2025-05-26 23:51:10 -0500

    updated homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Congrats on your first Vercel deployment and many more to come</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<body style="background-color: maroon; color: white; font-family: sans-serif; padding: 2rem;">
+	<h1>Congratulations on Your Graduation!</h1>
+	<p>
+		Visit
+		<a href="https://www.uchicago.edu" style="color: #ffd700; text-decoration: underline;">
+			the University of Chicago homepage
+		</a>
+		to celebrate your next chapter.
+	</p>
+</body>
