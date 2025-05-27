ad0a9488e90d0cf20f676f257e545b885d7d1b7e
ikeology <ikeology01123@gmail.com>
2025-05-26 23:58:43 -0500

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
-<body style="background-color: maroon; color: white; font-family: sans-serif; padding: 2rem;">
+<body
+	style="background-color: maroon; color: white; font-family: sans-serif; padding: 2rem; display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh; text-align: center;"
+>
 	<h1>Congratulations on Your Graduation!</h1>
 	<p>
 		Visit
-		<a href="https://www.uchicago.edu" style="color: #ffd700; text-decoration: underline;">
+		<a href="https://www.uchicago.edu" style="color: white; text-decoration: underline;">
 			the University of Chicago homepage
 		</a>
 		to celebrate your next chapter.
