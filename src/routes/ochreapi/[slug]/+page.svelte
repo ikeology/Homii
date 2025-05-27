0727226efafc3dbabc90b46e9a70e98477b83330
ikeology <ikeology01123@gmail.com>
2025-05-27 14:54:13 -0500

    dynamic routing

--- src/routes/ochreapi/[slug]/+page.svelte
+++ src/routes/ochreapi/[slug]/+page.svelte
@@ -1,83 +1,31 @@
 <script lang="ts">
-	import * as Table from '$lib/components/ui/table/index.js';
-	import { getPropertyByLabel, getUniquePropertyLabels } from '@digitalculture/ochre-sdk';
-	import { MapLibre, DefaultMarker } from 'svelte-maplibre';
-	const { data } = $props();
-	const setItems = data.set.items;
-	const propertyLabels = data.set.items.length
-		? getUniquePropertyLabels(data.set.items[0]!.properties)
-		: [];
+	const { item } = $props().data;
+	const items = item?.items ?? [];
 </script>
 
-<div class="map-style">
-	<MapLibre
-		zoom={4}
-		center={[33.9292, 36.0369]}
-		class="h-[400px]"
-		style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
-	>
-		{#each setItems as item}
-			{#if item.coordinates}
-				<DefaultMarker lngLat={[item.coordinates.longitude, item.coordinates.latitude]} />
-			{/if}
-		{/each}
-	</MapLibre>
-</div>
+{#each items as subItem}
 
-<div class="page-style">
-	<div class="table-style">
-		<Table.Root>
-			<Table.Caption>My OCHRE Items</Table.Caption>
-			<Table.Header>
-				<Table.Row>
-					<Table.Head class="w-[100px]">Name</Table.Head>
-					{#each propertyLabels as property}
-						<Table.Head>{property}</Table.Head>
-					{/each}
-				</Table.Row>
-			</Table.Header>
-			<Table.Body>
-				{#each setItems as item}
-					<Table.Row>
-						<Table.Cell>{item.identification.label}</Table.Cell>
-						{#each propertyLabels as propertyLabel}
-							<Table.Cell>{getPropertyByLabel(item.properties, propertyLabel)}</Table.Cell>
-						{/each}
-						<Table.Cell>{item.identification.label}</Table.Cell>
-					</Table.Row>
-				{/each}
-			</Table.Body>
-		</Table.Root>
-	</div>
-</div>
+<h2>{subItem.identification.label}</h2>
+
+<ul> {#each subItem.properties as prop}
+	<li>{prop.label}:{prop.values[0]?.content ?? 'null'}</li>
+{/each}
+</ul>
+
 
 <style>
-	.page-style {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		min-height: 100vh;
-		color: white;
-		background-color: white;
-		padding: 2rem;
+	h2 {
+		margin-top: 1.5rem;
+		font-size: 1.25rem;
+		color: #2c3e50;
 	}
 
-	.table-style {
-		width: 100%;
-		max-width: 700px;
-		color: black;
+	ul {
+		padding-left: 1rem;
+		margin-bottom: 2rem;
 	}
 
-	.map-style {
-		margin: 2rem auto;
-		max-width: 90%;
-		width: 100%;
-		height: 400px;
-		align-items: center;
-		justify-content: center;
-		border: 2px solid #2f4f4f;
-		border-radius: 12px;
-		overflow: hidden;
-		position: relative;
+	li {
+		margin-bottom: 0.5rem;
 	}
 </style>
