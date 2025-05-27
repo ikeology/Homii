1d69d44dedcf152b20dd4b9d37fa1e4b1e7d8ae8
ikeology <ikeology01123@gmail.com>
2025-05-26 23:48:39 -0500

    vercel deployment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>OCHRESDK Assignment - Welcome to SvelteKit</h1>
+<h1>Congrats on your first Vercel deployment and many more to come</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
